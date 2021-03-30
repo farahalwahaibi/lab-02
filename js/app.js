77e302c3812dae62ecb2c196090a674397825347
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-03-30 22:33:59 +0300

    lab-03 done

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 'use strict';
 let keyword = [];
-function Option(option) {
-    this.imageUrl = option.image_url;
-    this.title = option.title;
-    this.description = option.description;
-    this.keyword = option.keyword;
-    this.horns = option.horns;
+
+function Option(optionPar) {
+    this.imageUrl = optionPar.image_url;
+    this.title = optionPar.title;
+    this.description = optionPar.description;
+    this.keyword = optionPar.keyword;
+    this.horns = optionPar.horns;
     if (!keyword.includes(this.keyword)) {
         keyword.push(this.keyword);
     }
 }
 console.log(keyword);
 
+
+//get data from page-1
 $.ajax('data/page-1.json')
     .then(data => {
         let newOption ;
@@ -19,18 +22,30 @@ $.ajax('data/page-1.json')
             newOption = new Option(val);
             newOption.renderFilterList();
         });
+        $('#photo-template').first().remove();
+        forSelection ();
+        checkGallery();
     });
 
+//get data from page-2
+$.ajax('data/page-2.json')
+    .then(data => {
+        let newOption ;
+        data.forEach(val =>{
+            newOption = new Option(val);
+            newOption.createMustacheTemp();
 
+        });
+        // $('#photo-template2').first().remove();
+        // forSelection ();
+        checkGallery();
+    })
 
-Option.prototype.renderFilterList = function () {
-    let optionClone = $('option').first().clone().text(this.keyword);
-
-    $('select').append(optionClone);
-
-
-    // $('option').on('click',function(){
+   // option one to clone the div element from html 
+   Option.prototype.renderFilterList = function () {
     let div = $('#photo-template').clone();
+    div.addClass(this.keyword);
+    console.log(div);
     $('main').append(div);
 
     div.find('h2').text(this.title);
@@ -38,5 +53,49 @@ Option.prototype.renderFilterList = function () {
     div.find('img').attr('src', this.imageUrl);
 
     div.find('p').text(this.description);
-    // });
+    
 };
+
+
+   //option two to get the template from html and merge it to constructor
+   Option.prototype.createMustacheTemp = function () {
+       $('#page2').on('click',function () {
+       //1st step getting template by :
+       let template = $('#divTemplate').html();
+       
+       
+
+       //2nd step merging the data from constructor to the template :
+       let data = Mustache.render(template,this);
+
+       //add class
+       data.addClass(this.keyword);
+
+       //3rd return data :
+       return data ;
+    });
+   };
+
+   
+
+function checkGallery() {
+    $('select').change(function(){
+        let selectItem = $(this).val();
+        $('div').hide();
+        console.log(JSON.stringify(selectItem));
+        $(`.${selectItem}`).show();
+       console.log(selectItem);
+        
+    });
+}
+
+function forSelection () {
+   for (let index = 0; index < keyword.length; index++) {
+   let optionClone = $('option').first().clone().text(keyword[index]);
+    optionClone.attr('value', keyword[index]);
+    
+    $('select').append(optionClone);
+   }
+   
+}
+
