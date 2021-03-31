1faa041a65d3508e813931694d69094077269cfe
farahalwahaibi <farah.alwahaibi77@gmail.com>
2021-03-31 20:45:26 +0300

    lab-03 done

--- js/app.js
+++ js/app.js
@@ -1,101 +1,101 @@
 'use strict';
+
+// global array to push the keyword inside it:
 let keyword = [];
 
+// main constructor:
 function Option(optionPar) {
     this.imageUrl = optionPar.image_url;
     this.title = optionPar.title;
     this.description = optionPar.description;
     this.keyword = optionPar.keyword;
     this.horns = optionPar.horns;
+
+    // To check the keyword array data: 
     if (!keyword.includes(this.keyword)) {
         keyword.push(this.keyword);
     }
 }
-console.log(keyword);
 
+// For fill the dropdown list from the global array:
+let optionClone;
+function forSelection() {
+    for (let index = 0; index < keyword.length; index++) {
+        optionClone = $('option').first().clone().text(keyword[index]);
+        optionClone.attr('value', keyword[index]);
+        $('select').append(optionClone);
+    }
 
-//get data from page-1
-$.ajax('data/page-1.json')
+}
+
+// To show the specific data for selection keyword (FILTER):
+function checkGallery() {
+    $('select').change(function () {
+        let select = $(this).val();
+        $('div').hide();
+        $(`.${select}`).show();
+        console.log(select);
+    });
+}
+
+//Get data from page-1 to render the default page:
+$.ajax('./Data/page-1.json')
     .then(data => {
-        let newOption ;
         data.forEach(val => {
-            newOption = new Option(val);
-            newOption.renderFilterList();
+            let newOption = new Option(val);
+            newOption.renderMustache();
         });
-        $('#photo-template').first().remove();
-        forSelection ();
+        forSelection();
         checkGallery();
     });
 
-//get data from page-2
-$.ajax('data/page-2.json')
-    .then(data => {
-        let newOption ;
-        data.forEach(val =>{
-            newOption = new Option(val);
-            newOption.createMustacheTemp();
+// To get the template from html and merge it to constructor (CREATE DIV ELEMENT): 
+Option.prototype.renderMustache = function () {
+    //1st step getting template by :
+    let template = $('#galleryTemplate').html();
+    //2nd step merging the data from constructor to the template :
+    let data = Mustache.render(template, this);
 
-        });
-        // $('#photo-template2').first().remove();
-        // forSelection ();
-        checkGallery();
-    })
-
-   // option one to clone the div element from html 
-   Option.prototype.renderFilterList = function () {
-    let div = $('#photo-template').clone();
-    div.addClass(this.keyword);
-    console.log(div);
-    $('main').append(div);
-
-    div.find('h2').text(this.title);
-
-    div.find('img').attr('src', this.imageUrl);
-
-    div.find('p').text(this.description);
-    
+    $('select').append(optionClone);
+    $('main').append(data);
 };
 
+// When you click on button page1:
+$('#page1').on('click', function () {
+    $('main').empty();
+    $('select').empty();
+    keyword = [];
 
-   //option two to get the template from html and merge it to constructor
-   Option.prototype.createMustacheTemp = function () {
-       $('#page2').on('click',function () {
-       //1st step getting template by :
-       let template = $('#divTemplate').html();
-       
-       
+    $.ajax('./Data/page-1.json')
+        .then(data => {
+            data.forEach(val => {
+                let newOption = new Option(val);
+                newOption.renderMustache();
+            });
+            forSelection();
+            checkGallery();
+        });
+});
 
-       //2nd step merging the data from constructor to the template :
-       let data = Mustache.render(template,this);
+// When you click on button page2
+$('#page2').on('click', function () {
+    $('main').empty();
+    $('select').empty();
+    keyword = [];
 
-       //add class
-       data.addClass(this.keyword);
+    //get data from page-2 
+    $.ajax('data/page-2.json')
+        .then(data => {
+            let newOption;
+            data.forEach(val => {
+                newOption = new Option(val);
+                newOption.renderMustache();
 
-       //3rd return data :
-       return data ;
-    });
-   };
+            });
+            checkGallery();
+            forSelection();
+        })
+});
 
-   
 
-function checkGallery() {
-    $('select').change(function(){
-        let selectItem = $(this).val();
-        $('div').hide();
-        console.log(JSON.stringify(selectItem));
-        $(`.${selectItem}`).show();
-       console.log(selectItem);
-        
-    });
-}
-
-function forSelection () {
-   for (let index = 0; index < keyword.length; index++) {
-   let optionClone = $('option').first().clone().text(keyword[index]);
-    optionClone.attr('value', keyword[index]);
-    
-    $('select').append(optionClone);
-   }
-   
-}
 
